be2bb68326663ccb8fea2d35137f89da0b651e5d
parseb <petra306@protonmail.com>
2025-05-13 23:51:19 +0300

    adds base events

--- ponder/src/index.ts
+++ ponder/src/index.ts
@@ -42,5 +42,29 @@ ponder.on("Membranes-11155420:MembraneCreated", MembranesHandlers.handleMembrane
 // Note: Enable this once you have the PriceUpdate event in your contract
 // ponder.on("Will_11155420:PriceUpdate", WillHandlers.handlePriceUpdate);
 
+// For Base Mainnet (chainId 8453)
+ponder.on("WillWe-8453:NewRootNode", WillWeHandlers.handleNewRootNode);
+ponder.on("WillWe-8453:NewNode", WillWeHandlers.handleNewNode);
+ponder.on("WillWe-8453:MembershipMinted", WillWeHandlers.handleMembershipMinted);
+ponder.on("WillWe-8453:UserNodeSignal", WillWeHandlers.handleUserNodeSignal);
+ponder.on("WillWe-8453:CreatedEndpoint", WillWeHandlers.handleCreatedEndpoint);
+ponder.on("WillWe-8453:MembraneChanged", WillWeHandlers.handleMembraneChanged);
+ponder.on("WillWe-8453:InflationRateChanged", WillWeHandlers.handleInflationRateChanged);
+ponder.on("WillWe-8453:SharesGenerated", WillWeHandlers.handleSharesGenerated);
+ponder.on("WillWe-8453:Minted", WillWeHandlers.handleMinted);
+ponder.on("WillWe-8453:Burned", WillWeHandlers.handleBurned);
+ponder.on("WillWe-8453:MembraneSignal", WillWeHandlers.handleMembraneSignal);
+ponder.on("WillWe-8453:InflationSignal", WillWeHandlers.handleInflationSignal);
+
+// Execution handlers for Base Mainnet
+ponder.on("Execution-8453:NewMovementCreated", ExecutionHandlers.handleNewMovementCreated);
+ponder.on("Execution-8453:QueueExecuted", ExecutionHandlers.handleQueueExecuted);
+ponder.on("Execution-8453:NewSignaturesSubmitted", ExecutionHandlers.handleNewSignaturesSubmitted);
+ponder.on("Execution-8453:SignatureRemoved", ExecutionHandlers.handleSignatureRemoved);
+ponder.on("Execution-8453:WillWeSet", ExecutionHandlers.handleWillWeSet);
+
+// Membrane handlers for Base Mainnet
+ponder.on("Membranes-8453:MembraneCreated", MembranesHandlers.handleMembraneCreated);
+
 // Log which events are being indexed
-console.log("Registering event handlers for WillWe and Execution contracts on OP Sepolia");
+console.log("Registering event handlers for WillWe and Execution contracts on OP Sepolia and Base Mainnet");
